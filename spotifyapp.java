4d34421ee8bff7fc2aeb8ab06f1da2dd147d9eca
Louis ArmstrongII <louis2sivol@gmail.com>
2025-11-23 12:57:23 -0600

    main app to cycle through commmands

---
+++ spotifyapp.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class spotifyapp{
+
+    public static void main(String[] args){
+        boolean running = true; 
+        Scanner scanner = new Scanner(System.in); 
+        while(running = true){
+             String input = scanner.nextLine().toLowerCase(); 
+                System.out.println("Enter in your command "); 
+            switch(input){
+               
+                case "command1": 
+                    
+                break; 
+                
+                
+                case "command2":
+                    
+                break; 
+            
+            
+            
+            
+            
+            
+            }
+
+
+        }
+    }
+
+}
