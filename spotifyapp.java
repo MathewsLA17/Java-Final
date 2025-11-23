25417e67af423acfb677793a23e98491c3c434e9
Louis ArmstrongII <louis2sivol@gmail.com>
2025-11-23 13:07:53 -0600

    Update spotifyapp.java

--- spotifyapp.java
+++ spotifyapp.java
@@ -1,22 +1,40 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 public class spotifyapp{
 
     public static void main(String[] args){
         boolean running = true; 
         Scanner scanner = new Scanner(System.in); 
         while(running = true){
-             String input = scanner.nextLine().toLowerCase(); 
-                System.out.println("Enter in your command "); 
+            System.out.println("Command list command1 = printoutall"); 
+            String input = scanner.nextLine().toLowerCase();    
+            System.out.println("Enter in your command "); 
+              
+                
+                // thinking about using array list becauase the list of thigns might chnage not sure if that
+                //is the right play, here and I'm not sure what to putt into the array list? idk if we can 
+                //just pass in our file reader?? 
             switch(input){
-               
-                case "command1": 
-                    
+         
+            case "command1": 
+                    ArrayList<filereader> library = new ArrayList<>();
+      
+            for( library library: filereader ){
+            
+                persons.add(person); 
+                System.out.println(person.getName()); 
                 break; 
+            }
                 
-                
-                case "command2":
+            case "command2":
                     
-                break; 
+            break; 
             
             
             
